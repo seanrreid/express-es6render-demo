d30d7e76d90bd7b87e3684df06183f1d26966d4f
Sean R Reid <sean@torchcodelab.com>
2019-06-04 13:25:04 -0400

    Updated Rangers list to show routes

--- routes/rangers.js
+++ routes/rangers.js
@@ -2,12 +2,22 @@ const express = require('express'),
     router = express.Router();
 
 const rangerList = [
-    {name: 'Jason', rank: 'Red Ranger'},
-    {name: 'Billy', rank: 'Blue Ranger'},
-    {name: 'Zack', rank: 'Black Ranger'},
-    {name: 'Kim', rank: 'Pink Ranger'},
-    {name: 'Trini', rank: 'Yellow Ranger'}
+    {name: 'Jason', rank: 'Red Ranger', link: '/rangers/red_ranger'},
+    {name: 'Billy', rank: 'Blue Ranger', link: 'https://google.com/search?q=blue+ranger'},
+    {name: 'Zack', rank: 'Black Ranger', link: 'https://google.com/search?q=black+ranger'},
+    {name: 'Kim', rank: 'Pink Ranger', link: 'https://google.com/search?q=pink+ranger'},
+    {name: 'Trini', rank: 'Yellow Ranger', link: 'https://google.com/search?q=yellow+ranger'}
 ]
+
+function changeName(id) {
+    let name = '';
+    if (id === 'red_ranger') {
+        name = 'Red Ranger';
+    } else {
+        name = `No color given, you're not a ranger!`;
+    }
+    return name;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,6 +30,14 @@ router.get('/', function(req, res, next) {
             partial: 'partial-rangers'
         }
     });
+});
+
+router.get('/:ranger_name', (req, res, next) => {
+    console.log(req.params);
+    const rangerName = req.params.ranger_name;
+    
+    res.send(changeName(rangerName)).end();
+
 });
 
 module.exports = router;
